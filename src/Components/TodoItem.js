0a933b5a76d3e0b170ca721a86e00403f02e199f
IvanKahinga <ikahinga@gmail.com>
2023-01-11 12:26:15 +0300

    Initialized git repo and created required components

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,16 @@
+function TodoItem() {
+    return(
+        <div>
+        
+        <h1>Learn react</h1>
+        <p>
+            lorem ipsum
+        </p>
+            
+        </div>
+    )
+    
+}
+
+export default TodoItem
+
---
+++ src/Components/TodoList.js
@@ -0,0 +1,15 @@
+import React from "react"
+import TodoItem from "./todo-item"
+
+function TodoList() {
+   
+    return (
+    <>
+        <TodoItem/>
+        <TodoItem/>
+    </>
+          
+               )
+    }
+
+export default TodoList
